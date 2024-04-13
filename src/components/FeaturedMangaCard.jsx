cd29b5e6b60421dc2191f4c9a064d2cca0015274
Luis Choque <luis.choque.castro@outlook.com>
2024-04-13 11:53:56 -0500

    Fixed featured banner

--- src/components/FeaturedMangaCard.jsx
+++ src/components/FeaturedMangaCard.jsx
@@ -21,8 +21,18 @@ export function FeaturedMangaCard({ manga, ...props }) {
         <div {...props}>
             <a href={manga?.slug ? `/manga/${manga?.slug}` : '#'}>
                 <div className='relative w-full h-full group overflow-hidden rounded-md shadow-md hover:scale-[101%] transition-transform duration-300'>
-                    <img src={manga?.bannerUrl || manga?.imageUrl} className='absolute top-1/2 -translate-y-1/2 min-h-full min-w-full object-cover blur-sm duration-300 group-hover:scale-[104%] transition-transform' />
-                    <img src={manga?.bannerUrl || manga?.imageUrl} className='absolute inset-0 h-full mx-auto object-cover duration-300 group-hover:scale-[102%] transition-transform' />
+                    {!manga?.bannerUrl && (
+                        <img src={manga?.imageUrl} className='absolute top-1/2 -translate-y-1/2 min-h-full min-w-full object-cover blur-sm duration-300 group-hover:scale-[104%] transition-transform' />
+                    )}
+                    {!manga?.bannerUrl && (
+                        <img src={manga?.imageUrl} className='absolute inset-0 h-full mx-auto object-cover duration-300 group-hover:scale-[102%] transition-transform' />
+                    )}
+                    {manga?.bannerUrl && (
+                        <img src={manga?.bannerUrl} className='absolute top-1/2 -translate-y-1/2 min-h-full min-w-full object-cover blur-sm duration-300 group-hover:scale-[104%] transition-transform' />
+                    )}
+                    {manga?.bannerUrl && (
+                        <img src={manga?.bannerUrl} className='absolute inset-0 w-full min-h-full min-w-full max-h-full mx-auto object-cover duration-300 group-hover:scale-[102%] transition-transform' />
+                    )}
                     <div className="to-bg-black-10 absolute bottom-0 h-2/4 lg:h-3/4 w-full bg-gradient-to-t from-black/90 via-black/60 lg:via-black/80 blur-sm" />
                     <div className="absolute top-4 right-4 justify-end">
                         {/* if lastChapterAt was released in the last 3 days show a chip */}
