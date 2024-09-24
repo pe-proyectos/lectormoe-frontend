21bb018ecf37d54b2cc20512213dc6336cee0c6a
Yoider Flores <yoiderdf@gmail.com>
2024-09-24 20:57:25 -0300

    feat: added image and no_description

--- src/components/ProfileView.jsx
+++ src/components/ProfileView.jsx
@@ -26,8 +26,6 @@ export function ProfileView({ organization, member, userSlug, username }) {
   const [showAllChapterHistoryList, setShowAllChapterHistoryList] =
     useState(false);
 
-
-
   useEffect(() => {
     refreshUserChapterHistory();
     callAPI("/api/analytics", {
@@ -63,10 +61,7 @@ export function ProfileView({ organization, member, userSlug, username }) {
             <div className="flex flex-col items-center text-center">
               <img
                 className="w-24 h-24 mb-3 rounded-full shadow-lg"
-                src={
-                  member.imageUrl ||
-                  "https://i.pinimg.com/736x/0f/8b/73/0f8b739199c93a036ff8fab7a00c5caf.jpg"
-                }
+                src={member.imageUrl || "/public/images/profile-user2.png"}
                 alt={username}
               />
               <h5 className="text-xl font-medium text-white dark:text-white">
@@ -81,89 +76,90 @@ export function ProfileView({ organization, member, userSlug, username }) {
                 </span>
               </div>
               <p className="mb-4 text-white my-5">
-                {pascalCase(member.description)}
+                {member.description || _("no_description")}
               </p>
             </div>
           </div>
         </div>
 
-
-        <div className="w-full max-w-6xl grid grid-cols-3 gap-4 p-5">
-
-          <div className="border border-gray-200 rounded-lg p-4 text-center">
-            <h3 className="text-lg font-bold mb-2">Leer después</h3>
+        <div className="w-full max-w-6xl grid grid-cols-1 lg:grid-cols-3 gap-4 p-5">
+          <div>
+            <div className="border border-gray-200 rounded-lg p-4 text-center mb-2">
+              <h3 className="text-lg font-bold mb-2">Leer después</h3>
+            </div>
           </div>
-          <div className="border border-gray-200 rounded-lg p-4 text-center">
-            <h3 className="text-lg font-bold mb-2">Mis favoritos</h3>
+          <div>
+            <div className="border border-gray-200 rounded-lg p-4 text-center mb-2">
+              <h3 className="text-lg font-bold mb-2">Mis favoritos</h3>
+            </div>
           </div>
-          <div className="border border-gray-200 rounded-lg p-4 text-center">
-            <h3 className="text-lg font-bold mb-2">Continuar leyendo</h3>
-          </div>
-
-          <div className="border border-gray-200 rounded-lg p-4"></div>
-          <div className="border border-gray-200 rounded-lg p-4"></div>
-          <div className=" rounded-lg p-4">
-            <div className="flex flex-wrap gap-4 justify-center mb-4">
-              {!loadingHistory && userChapterHistoryList.length === 0 && (
-                <div className="flex flex-col items-center justify-center text-center w-full h-full">
-                  <Typography color="gray" className="font-light text-xl">
-                    {_("no_history")}
-                  </Typography>
-                </div>
-              )}
-              {(showAllChapterHistoryList
-                ? userChapterHistoryList
-                : userChapterHistoryList.slice(0, 6)
-              ).map((history) => (
-                <a
-                  key={history.id}
-                  className="w-full"
-                  href={`/manga/${history.chapter.mangaCustom.manga.slug}/chapters/${history.chapter.number}?page=${history.pageNumber}`}
-                >
-                  <div
-                    variant="ghost"
-                    className="w-full p-2  rounded-sm border-l-4 border-white bg-white hover:bg-opacity-10 bg-opacity-5"
+          <div>
+            <div className="border border-gray-200 rounded-lg p-4 text-center mb-2">
+              <h3 className="text-lg font-bold mb-2">Continuar leyendo</h3>
+            </div>
+            <div className=" rounded-lg p-4">
+              <div className="flex flex-wrap gap-4 justify-center mb-4">
+                {!loadingHistory && userChapterHistoryList.length === 0 && (
+                  <div className="flex flex-col items-center justify-center text-center w-full h-full">
+                    <Typography color="gray" className="font-light text-xl">
+                      {_("no_history")}
+                    </Typography>
+                  </div>
+                )}
+                {(showAllChapterHistoryList
+                  ? userChapterHistoryList
+                  : userChapterHistoryList.slice(0, 6)
+                ).map((history) => (
+                  <a
+                    key={history.id}
+                    className="w-full"
+                    href={`/manga/${history.chapter.mangaCustom.manga.slug}/chapters/${history.chapter.number}?page=${history.pageNumber}`}
                   >
-                    <div className="flex w-full items-center">
-                      <img
-                        src={
-                          history.chapter.imageUrl ||
-                          history.chapter.mangaCustom.imageUrl
-                        }
-                        alt=""
-                        className="mr-4 w-32 h-32 rounded-lg object-contain"
-                      />
-                      <div className="flex flex-col h-full">
-                        <p className="font-light text-xs uppercase">
-                          {history.chapter.mangaCustom.title}
-                        </p>
-                        <p className="font-medium text-xl">
-                          {history.chapter.title}
-                        </p>
-                        <p className="text-xs">
-                          {_("chapter")} {history.chapter.number}, {_("page")}{" "}
-                          {history.pageNumber}
-                        </p>
-                        <div className="grow justify-items-end">
-                          <p className="text-xs mt-auto">
-                            {_("seen")} {formatDate(history.lastReadAt)}
+                    <div
+                      variant="ghost"
+                      className="w-full p-2  rounded-sm border-l-4 border-white bg-white hover:bg-opacity-10 bg-opacity-5"
+                    >
+                      <div className="flex w-full items-center">
+                        <img
+                          src={
+                            history.chapter.imageUrl ||
+                            history.chapter.mangaCustom.imageUrl
+                          }
+                          alt=""
+                          className="mr-4 w-32 h-32 rounded-lg object-contain"
+                        />
+                        <div className="flex flex-col h-full">
+                          <p className="font-light text-xs uppercase">
+                            {history.chapter.mangaCustom.title}
                           </p>
+                          <p className="font-medium text-xl">
+                            {history.chapter.title}
+                          </p>
+                          <p className="text-xs">
+                            {_("chapter")} {history.chapter.number}, {_("page")}{" "}
+                            {history.pageNumber}
+                          </p>
+                          <div className="grow justify-items-end">
+                            <p className="text-xs mt-auto">
+                              {_("seen")} {formatDate(history.lastReadAt)}
+                            </p>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                </a>
-              ))}
-              {userChapterHistoryList.length > 6 && (
-                <Button
-                  color="gray"
-                  onClick={() =>
-                    setShowAllChapterHistoryList(!showAllChapterHistoryList)
-                  }
-                >
-                  {showAllChapterHistoryList ? _("see_less") : _("see_more")}
-                </Button>
-              )}
+                  </a>
+                ))}
+                {userChapterHistoryList.length > 6 && (
+                  <Button
+                    color="gray"
+                    onClick={() =>
+                      setShowAllChapterHistoryList(!showAllChapterHistoryList)
+                    }
+                  >
+                    {showAllChapterHistoryList ? _("see_less") : _("see_more")}
+                  </Button>
+                )}
+              </div>
             </div>
           </div>
         </div>
