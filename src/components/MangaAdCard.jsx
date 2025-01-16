d1a1645f6b82eeddf650991230daead7b675dbe0
Luis Choque <luis.choque.castro@outlook.com>
2025-01-15 22:09:11 -0500

    feat: Improved login to view message

--- src/components/MangaAdCard.jsx
+++ src/components/MangaAdCard.jsx
@@ -56,46 +56,51 @@ export function MangaAdCard({
         />
       </CardHeader>
       <CardBody>
-        <Typography variant="h6" color="gray" className="mb-4 uppercase">
-          {manga.title} #{chapter.number}
-        </Typography>
-        <Typography variant="h4" color="blue-gray" className="mb-2">
-          {chapter.title}
-        </Typography>
-        <Typography color="gray" className="mb-8 font-normal">
-          {manga.description}
-        </Typography>
-        <div>
-          {manga?.requireLogin && (
-            <Typography
-              variant="lead"
-              className="mb-4 font-bold text-black text-center"
-            >
-              {_("login_to_read_published_chapter")}
-            </Typography>
-          )}
-          <hr />
-          {getValidMangaSubscriptionPlans().length > 0 && (
-            <>
+        <div className="flex flex-col h-full">
+          <Typography variant="h6" color="gray" className="mb-4 uppercase">
+            {manga.title} #{chapter.number}
+          </Typography>
+          <Typography variant="h4" color="blue-gray" className="mb-2">
+            {chapter.title}
+          </Typography>
+          <Typography color="gray" className="mb-8 font-normal">
+            {manga.description}
+          </Typography>
+          <hr className="my-auto" />
+          <div className="my-auto">
+            {manga?.requireLogin && !logged && (
               <Typography
                 variant="lead"
-                className="my-4 font-bold text-black text-center"
+                className="font-bold text-black text-center"
               >
-                {_("subscribe_now_to_one_of_these_plans_to_read_chapter_early")}
+                {_("login_to_read_published_chapter")}
               </Typography>
+            )}
+            {getValidMangaSubscriptionPlans().length > 0 && logged && (
+              <>
+                <Typography
+                  variant="lead"
+                  className="font-bold text-black text-center"
+                >
+                  {_(
+                    "subscribe_now_to_one_of_these_plans_to_read_chapter_early"
+                  )}
+                </Typography>
 
-              <div className="flex flex-wrap gap-2 justify-center">
-                {getValidMangaSubscriptionPlans().map((plan) => (
-                  <div
-                    key={plan.id}
-                    className="text-sm font-extralight text-black bg-gray-500 bg-opacity-50 rounded-lg px-2 py-1"
-                  >
-                    {plan.name}
-                  </div>
-                ))}
-              </div>
-            </>
-          )}
+                <div className="flex flex-wrap gap-2 justify-center">
+                  {getValidMangaSubscriptionPlans().map((plan) => (
+                    <div
+                      key={plan.id}
+                      className="text-sm font-extralight text-black bg-gray-500 bg-opacity-50 rounded-lg px-2 py-1"
+                    >
+                      {plan.name}
+                    </div>
+                  ))}
+                </div>
+              </>
+            )}
+          </div>
+          <hr className="my-auto" />
         </div>
       </CardBody>
     </Card>
